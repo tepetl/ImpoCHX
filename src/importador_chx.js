ec5bb1f7f6cad888acab27ce7627af649314a96c
tepetl <alffore@yahoo.com.mx>
2016-03-02 18:32:53 -0600

    Se agrega la carpeta de fuentes (src) y el esqueleto b?sico de la aplicacion

---
+++ src/importador_chx.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+
+function leelinea(input,func){
+
+
+
+
+
+}
+
+function func(data){
+
+
+}
+
+
+
+
+process.argv.forEach(function(valor,index,array){
+
+	console.log(index+" "+valor);
+	
+/*	if(index>=2){
+		var  input =fs.createReadStream(val);
+		leelinea(input,func);
+	}*/
+});
+
