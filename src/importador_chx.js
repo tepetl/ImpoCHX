0d993404cc5b40809be13ee9c87eead22aa18255
alffore <alffore@gmail.com>
2016-03-02 19:01:27 -0600

    Se completa el codigo para probar la importacion de datos

--- src/importador_chx.js
+++ src/importador_chx.js
@@ -1,16 +1,35 @@
 var fs = require("fs");
 
-function leelinea(input,func){
+function leeLinea(input,func){
 
+	var resto= '';
 
+	input.on('data',function(data){
+		resto +=data;
+		var index = resto.indexOf('\n');
 
+		while(index > -1){
+			var linea = resto.substring(0,index);
+			resto=resto.substring(index+1);
+			func(linea);
+			index = resto.indexOf('\n');
+		}
 
+	});
+
+	input.on('end',function(){
+		if(resto.length >0){
+				func(resto);
+		}
+
+	});
 
 }
 
+
+
 function func(data){
-
-
+	console.log("Linea: "+data);
 }
 
 
@@ -19,10 +38,9 @@ function func(data){
 process.argv.forEach(function(valor,index,array){
 
 	console.log(index+" "+valor);
-	
-/*	if(index>=2){
-		var  input =fs.createReadStream(val);
-		leelinea(input,func);
-	}*/
-});
 
+	if(index>=2){
+		var  input =fs.createReadStream(valor);
+		leeLinea(input,func);
+	}
+});
