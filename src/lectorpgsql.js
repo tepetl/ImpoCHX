9394dd78d967760dba6197cf3dea90b5afda5e44
A. Alfonso F. R <alffore@gmail.com>
2016-03-03 22:36:35 -0600

    Continuan correcciones

--- src/lectorpgsql.js
+++ src/lectorpgsql.js
@@ -1,6 +1,6 @@
 var pg=require('pg');
 
-var connectionString = process.env.DATABASE_URL || 'postgres://127.0.0.1:5432/MONITOREO';
+var connectionString = process.env.DATABASE_URL || 'postgres://popoca:b0b054@127.0.0.1:5432/MONITOREO';
 
 var client = new pg.Client(connectionString);
 client.connect();
